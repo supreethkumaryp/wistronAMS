93c7a010c4ce5871d8ff999d63cca036abb26807
Supreeth Kumar Y P <supreethkumar.yp@gmail.com>
2021-01-05 23:06:30 +0530

    employees

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField, IntegerField, PasswordField, SelectField, DateField, FileField, HiddenField
+from wtforms import TextField, TextAreaField, IntegerField, FloatField, PasswordField, SelectField, DateField, FileField, HiddenField, BooleanField
 from wtforms.validators import InputRequired, DataRequired
 from flask_wtf.file import FileRequired
 
@@ -93,4 +93,164 @@ class ShiftForm(FlaskForm):
         'Shift Name',
         validators=[DataRequired()],
         render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+
+class UserForm(FlaskForm):
+    """User Form"""
+
+    usr_miId = TextField(
+        'MI ID',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_alternateMiId = TextField(
+        'Alternate MI ID',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_name = TextField(
+        'Name',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    dept_id = SelectField(
+        'Department',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    func_id = SelectField(
+        'Function',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_type = SelectField(
+        'Type',
+        choices=[("", "-- Select --"), ("dl", "DL"), ("idl", "IDL")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    plant_id = SelectField(
+        'Plant',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_designation = SelectField(
+        'Designation',
+        choices=[
+            ("", "-- Select --"),
+            ("em", "Engineering Manager"),
+            ("m", "Manager"),
+            ("am", "Assistant Manager"),
+            ("tl", "Team Leader"),
+            ("se", "Senior Engineer"),
+            ("je", "Junior Engineer"),
+            ("get", "GET"),
+            ("det", "DET"),
+            ("op", "Operator")
+        ],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_gender = SelectField(
+        'Gender',
+        choices=[("", "-- Select --"), ("male", "Male"), ("female", "Female")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    branch_id = SelectField(
+        'Branch',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_experience = FloatField(
+        'Experience',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_dob = DateField(
+        'Date of Birth',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    project_id = SelectField(
+        'Project',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_permanentAddress = TextAreaField(
+        'Permanent Address',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_currentAddress = TextAreaField(
+        'Current Address',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_pickUpRoute = TextField(
+        'PickUp Route',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_pickUpPoint = TextField(
+        'PickUp Point',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_contactNumber = TextField(
+        'Contact Number',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_emergencyContactNumber = TextField(
+        'Emergency Contact',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This Field is required"}
+    )
+    usr_bloodGroup = SelectField(
+        'Blood Group',
+        choices=[
+            ("", "-- Select --"),
+            ("A+", "A+"),
+            ("A-", "A-"),
+            ("B+", "B+"),
+            ("B-", "B-"),
+            ("O+", "O+"),
+            ("O-", "O-"),
+            ("AB+", "AB+"),
+            ("AB-", "AB-")
+        ],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_doj = DateField(
+        'Date of Joining',
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_status = SelectField(
+        'Status',
+        choices=[
+            ("", "-- Select --"),
+            ("active", "Active"),
+            ("inactive", "In-Active"),
+            ("abscond", "Abscond")
+        ],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    shift_id = SelectField(
+        'Shift',
+        choices=[("", "-- Select --")],
+        validators=[DataRequired()],
+        render_kw={"required":"true", "data-validation-required-message":"This field is required"}
+    )
+    usr_isProxy = BooleanField(
+        'Is Shift Incharge',
+        validators=[DataRequired()]
     )
--- app/home/routes.py
+++ app/home/routes.py
@@ -12,7 +12,10 @@ from datetime import date
 from app import db, login_manager
 from app.home import blueprint
 from app.home.models import Department, Function, Plant, Branch, Project, Shift
-from app.home.forms import DepartmentForm, FunctionForm, PlantForm, BranchForm, ProjectForm, ShiftForm
+from app.home.forms import DepartmentForm, FunctionForm, PlantForm, BranchForm, ProjectForm, ShiftForm, UserForm
+
+from app.auth.models import User
+from app.auth.utils import hash_pass
 
 from jinja2 import TemplateNotFound
 
@@ -189,3 +192,98 @@ def shift_create(shift_id):
         db.session.commit()
         return redirect(url_for('home_blueprint.shift'))
     return render_template('shift/create-edit-shift.html', form=form)
+
+# Shift
+@blueprint.route('/employee', methods=['GET', 'POST'])
+def employee():
+    return render_template('employee/employees.html')
+
+@blueprint.route('/get-employees', methods=['GET', 'POST'])
+def get_employees():
+    data = []
+    for record in db.session.query(User).join(Department, Department.dept_id == User.dept_id).join(Function, Function.func_id == User.func_id).join(Plant, Plant.plant_id == User.plant_id).join(Branch, Branch.branch_id == User.branch_id).join(Project, Project.project_id == User.project_id).join(Shift, Shift.shift_id == User.shift_id).all():
+        player_record = {
+            'usr_miId' : record.usr_miId,
+            'usr_alternateMiId' : record.usr_alternateMiId,
+            'usr_name' : record.usr_name,
+            'usr_department' : record.usr_department.dept_name,
+            'usr_function' : record.usr_function.func_name,
+            'usr_type' : record.usr_type.upper(),
+            'usr_plant' : record.usr_plant.plant_name,
+            'usr_designation' : record.usr_designation.upper(),
+            'usr_gender' : record.usr_gender,
+            'usr_branch' : record.usr_branch.branch_name,
+            'usr_project' : record.usr_project.project_name,
+            'usr_contactNumber' : record.usr_contactNumber,
+            'usr_emergencyContactNumber' : record.usr_emergencyContactNumber,
+            'usr_status' : record.usr_status,
+            'usr_shift' : record.usr_shift.shift_name,
+            'usr_isProxy' : record.usr_isProxy
+        }
+        data.append(player_record)
+    return jsonify(data=data)
+
+@blueprint.route('/employee/<usr_miId>', methods=['GET', 'POST'])
+def employee_create(usr_miId):
+    if (usr_miId == "New"):
+        form = UserForm(request.form)
+
+        form.usr_miId.render_kw['readonly'] = False
+
+        form.dept_id.choices += [(r.as_dict()["dept_id"], r.as_dict()["dept_name"]) for r in Department.query.all()]
+        form.func_id.choices += [(r.as_dict()["func_id"], r.as_dict()["func_name"]) for r in Function.query.all()]
+        form.plant_id.choices += [(r.as_dict()["plant_id"], r.as_dict()["plant_name"]) for r in Plant.query.all()]
+        form.branch_id.choices += [(r.as_dict()["branch_id"], r.as_dict()["branch_name"]) for r in Branch.query.all()]
+        form.project_id.choices += [(r.as_dict()["project_id"], r.as_dict()["project_name"]) for r in Project.query.all()]
+        form.shift_id.choices += [(r.as_dict()["shift_id"], r.as_dict()["shift_name"]) for r in Shift.query.all()]
+
+        if 'save' in request.form:
+            emp = User(**request.form)
+
+            emp.usr_password = hash_pass("Wistron@123")
+
+            db.session.add(emp)
+            db.session.commit()
+            return redirect(url_for('home_blueprint.employee'))
+        return render_template('employee/create-edit-employee.html', form=form)
+
+    emp = User.query.filter_by(usr_miId=usr_miId).first()
+    form = UserForm(obj=emp)
+
+    form.usr_miId.render_kw['readonly'] = True
+
+    form.dept_id.choices += [(r.as_dict()["dept_id"], r.as_dict()["dept_name"]) for r in Department.query.all()]
+    form.func_id.choices += [(r.as_dict()["func_id"], r.as_dict()["func_name"]) for r in Function.query.all()]
+    form.plant_id.choices += [(r.as_dict()["plant_id"], r.as_dict()["plant_name"]) for r in Plant.query.all()]
+    form.branch_id.choices += [(r.as_dict()["branch_id"], r.as_dict()["branch_name"]) for r in Branch.query.all()]
+    form.project_id.choices += [(r.as_dict()["project_id"], r.as_dict()["project_name"]) for r in Project.query.all()]
+    form.shift_id.choices += [(r.as_dict()["shift_id"], r.as_dict()["shift_name"]) for r in Shift.query.all()]
+    if 'save' in request.form:
+
+        emp.usr_alternateMiId = form.usr_alternateMiId.data
+        emp.usr_name = form.usr_name.data
+        emp.dept_id = form.dept_id.data
+        emp.func_id = form.func_id.data
+        emp.usr_type = form.usr_type.data
+        emp.plant_id = form.plant_id.data
+        emp.usr_designation = form.usr_designation.data
+        emp.usr_gender = form.usr_gender.data
+        emp.branch_id = form.branch_id.data
+        emp.usr_experience = form.usr_experience.data
+        emp.usr_dob = form.usr_dob.data
+        emp.project_id = form.project_id.data
+        emp.usr_permanentAddress = form.usr_permanentAddress.data
+        emp.usr_currentAddress = form.usr_currentAddress.data
+        emp.usr_pickUpRoute = form.usr_pickUpRoute.data
+        emp.usr_pickUpPoint = form.usr_pickUpPoint.data
+        emp.usr_contactNumber = form.usr_contactNumber.data
+        emp.usr_emergencyContactNumber = form.usr_emergencyContactNumber.data
+        emp.usr_bloodGroup = form.usr_bloodGroup.data
+        emp.usr_doj = form.usr_doj.data
+        emp.usr_status = form.usr_status.data
+        emp.shift_id = form.shift_id.data
+        emp.usr_isProxy = form.usr_isProxy.data
+
+        db.session.commit()
+        return redirect(url_for('home_blueprint.employee'))
+    return render_template('employee/create-edit-employee.html', form=form)
