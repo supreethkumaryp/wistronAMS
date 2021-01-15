89e4af2bbe173e3611a8a17abad6b3419cc1b5dc
Supreeth Kumar Y P <supreethkumar.yp@gmail.com>
2021-01-15 07:11:29 +0530

    MSI

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 
+from sys import exit
+
 from config import config_dict
 from app import create_app
 
-app_config = config_dict['Debug']
+from pyfladesk import init_gui
+
+app_config = config_dict['Production']
 
 app = create_app(app_config)
 
-app.run()
+init_gui(app, port=5000, width=1280, height=800, window_title="Wistron AMS", icon="app/base/static/app-assets/images/logo/android-chrome-512x512.png")
